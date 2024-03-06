4eed8031dcb04e628f39bf421d8763a3f7f66c97
Mario Lazzari <mario.lazzari@gmail.com>
2024-03-06 22:38:29 +0100

    Done lesson 2

--- src/app/RegistrationForm.tsx
+++ src/app/RegistrationForm.tsx
@@ -25,23 +25,59 @@ export const RegistrationForm = () => {
     },
   });
 
+  const onSubmt = async (data: z.infer<typeof schema>) => {
+    console.log(data);
+  };
+
   return (
     <Form {...form}>
-      <form className="space-y-8">
+      <form className="space-y-8" onSubmit={form.handleSubmit(onSubmt)}>
+        <div className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your email address</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="first"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>First</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your first name</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
         <FormField
           control={form.control}
-          name="email"
+          name="last"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Email</FormLabel>
+              <FormLabel>Last</FormLabel>
               <FormControl>
                 <Input placeholder="" {...field} />
               </FormControl>
-              <FormDescription>Your email address</FormDescription>
+              <FormDescription>Your last name</FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
