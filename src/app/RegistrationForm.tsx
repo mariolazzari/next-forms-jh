13b843ce5fdd4ccf2cb8ff04114d28e418e5d6dc
Mario Lazzari <mario.lazzari@gmail.com>
2024-03-06 22:53:50 +0100

    Done lesson 4

--- src/app/RegistrationForm.tsx
+++ src/app/RegistrationForm.tsx
@@ -26,7 +26,16 @@ export const RegistrationForm = () => {
   });
 
   const onSubmt = async (data: z.infer<typeof schema>) => {
-    console.log(data);
+    const res = await fetch("/api/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await res.json();
+
+    console.log(resData);
   };
 
   return (
